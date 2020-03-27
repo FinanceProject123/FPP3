44cca28483c9afb72f66adf8c3a641ab82d2d5dd
kongyao <kongyao_ustb@126.com>
2020-03-27 21:18:31 +0800

    框架搭建

--- FPP/spiders/quotes_spider.py
+++ FPP/spiders/quotes_spider.py
@@ -21,6 +21,7 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         page = response.url.split("/")[-2]
+
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
