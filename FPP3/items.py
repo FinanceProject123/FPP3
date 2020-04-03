2435f0ece0f4d3cddd437f7e92147ce1c68c7972
FinanceProject123 <financeproject123@163.com>
2020-04-03 17:05:06 +0800

    结构爬取

--- FPP3/items.py
+++ FPP3/items.py
@@ -5,10 +5,24 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+
+from scrapy.item import Item, Field
 
 
-class Fpp3Item(scrapy.Item):
+class Fpp3Item(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    para = scrapy.Field()
+    para = Field()
+
+
+class BaiduBaike(Item):
+    # 一级标题 如：玉米
+    title_h1 = Field()
+    # 二级标题 如：（玉米）种植技术
+    title_h2 = Field()
+    # 三级标题 如：（玉米）（种植技术）选用优良品种
+    title_h3 = Field()
+    # 主要内容
+    context = Field()
+    # 文章中的url字段，暂时不添加
+    pass
